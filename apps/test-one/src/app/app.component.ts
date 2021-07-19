1532265d1e098eace7bdc7a6dbcdcd85e32ea920
Erkan Buelbuel <erkan.buelbuel@bit.admin.ch>
2021-07-19 13:11:25 +0200

    initial commit

---
+++ apps/test-one/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-one-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-one';
+}
